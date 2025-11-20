324abfcb3cda7f77549daf20c809b7a438554ece
TromboneStevens <46182844+TromboneStevens@users.noreply.github.com>
2025-11-19 23:30:58 -0800

    not receiving packets

--- src/ready.lua
+++ src/ready.lua
@@ -7,14 +7,14 @@ local folder = get_script_path()
 package.path = folder .. "?.lua;" .. folder .. "src/?.lua;" .. package.path
 
 local NetworkManager = require("NetworkManager")()
-local socket = require("socket") -- Need direct access to socket for the test
 
 -- 1. Initialize Socket
 if config and config.mode == "host" then
-    NetworkManager.Init("host", 7777)
+    NetworkManager.Init("host", config.port or 7777)
 else
-    NetworkManager.Init("client", 7777)
-    NetworkManager.Connect("127.0.0.1", 7777) 
+    -- Use the IP from config.lua!
+    NetworkManager.Init("client", config.port or 7777)
+    NetworkManager.Connect(config.target_ip or "127.0.0.1", config.port or 7777) 
 end
 
 local is_network_thread_running = false
@@ -26,35 +26,20 @@ modutil.mod.Path.Wrap("SetupMap", function(base, ...)
     if is_network_thread_running then return end
     is_network_thread_running = true
 
-    print("[Hades2MP] SetupMap Hook Triggered.")
+    print("[Hades2MP] Network Thread Started.")
 
     thread(function()
+        -- Small delay to let the level settle
         wait(1.0) 
-        
-        -- [[ IMPROVED TEST LOGIC ]]
-        if config.mode == "host" then
-            print("[Hades2MP] Host Mode: Creating temporary client for handshake test...")
-            
-            -- Create a temporary "Fake Client" just to send a packet
-            local temp_client = socket.udp()
-            temp_client:setpeername("127.0.0.1", 7777)
-            local res, err = temp_client:send("HELLO FROM FAKE CLIENT")
-            
-            if res then
-                print("[Hades2MP] Test packet sent successfully.")
-            else
-                print("[Hades2MP] Test packet FAILED to send: " .. tostring(err))
-            end
-            
-            temp_client:close()
-        else
-            print("[Hades2MP] Client Mode: Sending packet to server...")
-            NetworkManager.SendString("CLIENT IN-GAME PACKET")
+
+        -- Just send one "I'm here" packet
+        if config.mode == "client" then
+             NetworkManager.SendString("Player Connected!")
         end
 
-        -- Network Loop
         while true do
             NetworkManager.Poll()
+            -- Keep the small wait to be safe on performance
             wait(0.1)
         end
     end)
